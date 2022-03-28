55e6cfc0d034d3ff355e5858446890526aa82382
mipoks <d_zakiev@mail.ru>
2022-03-28 18:43:52 +0300

    abstract class changed

--- src/main/java/design/kfu/sunrise/domain/model/embedded/ActiveInfo.java
+++ src/main/java/design/kfu/sunrise/domain/model/embedded/ActiveInfo.java
@@ -3,7 +3,9 @@ package design.kfu.sunrise.domain.model.embedded;
 import lombok.*;
 
 import javax.persistence.Embeddable;
+import javax.persistence.Transient;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Date;
 
 /**
@@ -15,10 +17,15 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
-public abstract class ActiveInfo implements Serializable {
-    private Date created;
+public class ActiveInfo implements Serializable {
+
+    private boolean expired;
+
+    private String expireCondition;
+
+    private Instant created;
     //Различные варианты: истечение по количеству участников, истечение по времени
-    public abstract boolean isExpired();
-    public abstract void setExpired(boolean expired);
-    public abstract String expireCondition();
+    public boolean isExpired() {
+        return expired;
+    }
 }
